6062f4076f74adbc3f6ef1eae4850f681cc528ff
Mohamed Amine GABSI <mamine.gabsi@gmail.com>
2025-05-30 12:17:49 -0400

    Converting original chrome extension to safari

---
+++ Prettify Minerva/Prettify Minerva/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  Prettify Minerva
+//
+//  Created by Mohamed Amine GABSI on 30/05/2025.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Override point for customization after application launch.
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+}
